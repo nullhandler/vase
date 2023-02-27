d7094dac2f69f609f6bc5cc4debcf8d26d9d7e9c
Selva <selva.kumar@coindcx.com>
2023-02-27 19:02:43 +0530

    init

---
+++ android/app/src/main/kotlin/com/github/nullhandler/vase/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.github.nullhandler.vase
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:vase/screens/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'Vase',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
